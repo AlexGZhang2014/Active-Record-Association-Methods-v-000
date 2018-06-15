ddad08e9c676a439890dc53f9c47ff683e8eab4b
Alex Zhang <alexgzhang92@gmail.com>
2018-06-15 22:10:30 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,3 +9,16 @@ class Song < ActiveRecord::Base
     # when this method is called it should assign the song's artist to Drake
   end
 end
+
+it '#get_genre_name' do
+    genre = Genre.create(name: "Rock")
+    @song.genre = genre
+    @song.save
+
+    expect(@song.get_genre_name).to eq("Rock")
+  end
+
+  it '#drake_made_this' do
+    expect(@song.drake_made_this).to be_a(Artist)
+    expect(@song.drake_made_this.name).to eq("Drake")
+  end
